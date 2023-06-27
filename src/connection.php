14921b864c93e2c97aa7121e85344c1ca4941604
Razvan Raducanu <rrazvan.rraducanu@gmail.com>
2023-06-27 10:00:01 +0300

    first commit

---
+++ src/connection.php
@@ -0,0 +1,3 @@
+<?php
+$con=mysqli_connect('mysql_db', 'root', 'toor', 'flowers') or die("Failed to connect: ". mysqli_error($con));
+?>
---
+++ src/view.php
@@ -0,0 +1,11 @@
+<?php
+        include 'connection.php';
+        $sql="SELECT * FROM flori WHERE id='{$_GET['id']}'";
+        $query=  mysqli_query($con,$sql)or die(mysqli_error($con));
+        $row=mysqli_fetch_array($query);
+             echo "Nume: ".$row["nume"]."<br/>";
+             echo "Culoare ".$row["culoare"]."<br/>";
+             echo "Marime: ".$row["marime"]."<br/>";
+             echo "Pret: ".$row["pret"]."<br/>";
+            ?>
+<a href="index.php">Back</a>
